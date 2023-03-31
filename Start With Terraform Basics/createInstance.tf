5d69d192e9874707d4afa8e91f6623dc17925492
FODavid <david.famuyide@gmail.com>
2023-03-31 22:51:44 +0100

    multiple instances

--- Start With Terraform Basics/createInstance.tf
+++ Start With Terraform Basics/createInstance.tf
@@ -1,6 +1,11 @@
 
 resource "aws_instance" "example" {
+    count = 3
     ami = "ami-0f29c8402f8cce65c"
     instance_type = "t2.micro"
+
+    tags {
+        Name = "threedemoinstance"
+    }
   
 }
